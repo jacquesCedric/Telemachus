0043ef4d2d1f65c7b1df9094b059b743ffc184e1
Jacques <jacquescedric@users.noreply.github.com>
2016-07-13 17:16:22 +1000

    Foundation
    First commit + initial readme

---
+++ README.txt
@@ -0,0 +1,10 @@
+Telemachus README
+
+This program is still being developed - and so is this documentation!
+
+This program aims to be a graphical front for ShellMS on Android. Built for macOS, users can select a contact on their computer, then send the message via their device.
+
+Requires that ShellMS is installed on your android device. Grab it via F-Droid or from source here: https://github.com/try2codesecure/ShellMS
+
+
+Licensed under the GPL v3, refer to LICENSE.txt for more information.
---
+++ Telemachus/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Telemachus
+//
+//  Created by Jacob Gaffney on 13/07/2016.
+//  Copyright © 2016 Jacob Gaffney. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
