2cdafd7e4d93906ff2c2266f1decaa0bd2da2377
Jacques <jacquescedric@users.noreply.github.com>
2016-07-15 14:24:35 +1000

    Contacts Data
    Contacts data is now properly stored in a sorted dictionary. Added some
    comments throughout the code to make it a bit more legible (though it
    still needs a cleanup once functionality is complete)

--- Telemachus/ViewController.swift
+++ Telemachus/ViewController.swift
@@ -17,15 +17,34 @@ class ViewController: NSViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        // Make sure our textfields' purpose is clear to the user
         numberField.placeholderString = "Phone Number"
         messageField.placeholderString = "Write a Message..."
         
+        
+        // This will be our dictionary for names and numbers
+        var namesAndNumbers = [String: String]()
+        
+        // This block makes sure we only collect mobile numbers for our list
         for contact in contacts{
             if contact.phoneNumbers.first?.value != nil {
+                let phoneNumber = CommunicationTools.validateNumber((contact.phoneNumbers.first?.value as? CNPhoneNumber)!.stringValue)
                 let fullname = CNContactFormatter.stringFromContact(contact, style: .FullName)
-                print("\((contact.phoneNumbers.first?.value as? CNPhoneNumber)!.stringValue) - \(fullname!)")
+                
+                // Only mobile numbers, thanks!
+                if phoneNumber.characters.count > 8{
+                    namesAndNumbers[fullname!] = phoneNumber
+                }
             }
+        }
+        
+        // Create a dictionary that's sorted alphabetically by key and includes values
+        let sortedNamesAndNumbers = namesAndNumbers.sort { $0.0 < $1.0}
+        
+        // Just a test
+        for (name, number) in sortedNamesAndNumbers {
+            print("\(name) - \(number)")
         }
     }
 
@@ -38,17 +57,13 @@ class ViewController: NSViewController {
     
     // What happens when you click the send button
     @IBAction func sendMessage(sender: NSButton) {
+        // Send our SMS
         CommunicationTools.smsCommand(numberField.stringValue, messageTextField: messageField.stringValue)
         
+        // Clean up the form
         print("clearing inputs")
         clearFields()
     }
-
-    func clearFields() {
-        numberField.stringValue = ""
-        messageField.stringValue = ""
-    }
-    
     
     // Let's collect our contacts from different containers
     lazy var contacts: [CNContact] = {
@@ -57,6 +72,7 @@ class ViewController: NSViewController {
             CNContactFormatter.descriptorForRequiredKeysForStyle(.FullName),
             CNContactPhoneNumbersKey]
         
+        // Collect all containers, in the event user has more than one
         var allContainers: [CNContainer] = []
         do {
             allContainers = try contactStore.containersMatchingPredicate(nil)
@@ -64,25 +80,31 @@ class ViewController: NSViewController {
             print("Error fetching containers")
         }
         
+        // Collect all the contacts from each of these containers
         var results: [CNContact] = []
-        
         for container in allContainers {
             let fetchPredicate = CNContact.predicateForContactsInContainerWithIdentifier(container.identifier)
             
             do {
                 let containerResults = try contactStore.unifiedContactsMatchingPredicate(fetchPredicate, keysToFetch: keysToFetch)
                 
-                //print("\(containerResults)")
-                
+                // Collect the contact information we need
                 results.appendContentsOf(containerResults)
             } catch {
                 print("Error fetching results for container")
             }
         }
-        
+        // This value should be all of our contacts, albeit unformatted and full of extra stuff
         return results
-        
     }()
+    
+    
+    
+    // Clean up fields
+    func clearFields() {
+        numberField.stringValue = ""
+        messageField.stringValue = ""
+    }
     
 }
 
